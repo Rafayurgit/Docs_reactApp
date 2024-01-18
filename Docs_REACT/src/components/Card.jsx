f59315ac6ee46e82f2f0207922a13cce619661e4
farruop <farhankhanpvt00@gmail.com>
2024-01-18 22:34:53 +0530

    Added props to card and foreground

--- Docs_REACT/src/components/Card.jsx
+++ Docs_REACT/src/components/Card.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { LuDownload } from "react-icons/lu";
 import { FaRegFileAlt } from "react-icons/fa";
+import {IoClose} from "react-icons/io5";
 
 
-function Card() {
+function Card({data}) {
   return (
-    <div className='relative overflow-hidden w-60 h-72 bg-zinc-900/90 rounded-[20px] text-white p-5'>
+    <div className='relative flex-shrink-0 overflow-hidden w-60 h-72 bg-zinc-900/90 rounded-[20px] text-white p-5'>
         <FaRegFileAlt/>
-        <p className='text-sm leading-tight mt-5 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro non culpa molestiae quaerat!</p>
+        <p className='text-sm leading-tight mt-5 font-semibold'>{data.desc}</p>
         <div className=' footer absolute w-full bottom-0 left-0'>
             <div className='flex items-center justify-between px-8 py-3 mb-3'>
-            <h3>.4mb</h3>
+            <h3>{data.fileSize}</h3>
             <span className='w-6 h-6 bg-zinc-600 rounded-full flex justify-center items-center'>
-            <LuDownload size=".7em" color='#fff'/>
+                {data.close ? <IoClose/> :<LuDownload size=".7em" color='#fff'/>}
             </span> 
             </div>
-            <div className='tag w-full h-9 bg-sky-400 flex justify-center items-center text-sm'>Download Now</div> 
+            {data.tag.isOpen ? <div className='tag w-full h-9 bg-sky-400 flex justify-center items-center text-sm'>Download Now</div> : null}
+             
         </div>
     </div>
   )
--- Docs_REACT/src/components/Foreground.jsx
+++ Docs_REACT/src/components/Foreground.jsx
@@ -2,10 +2,36 @@ import React from 'react'
 import Card from './Card'
 
 function Foreground() {
+
+    const data =[
+        {
+            desc:"This is just a random description for the project",
+            fileSize: ".9mb",
+            close: true,
+            tag:{
+                isOpen:false , tagTitle:"Download Now", tagColor:"green"},
+        },
+        {
+            desc:"This is just a random description for the project",
+            fileSize: ".9mb",
+            close: true,
+            tag:{
+                isOpen:false , tagTitle:"Download Now", tagColor:"green"},
+        },
+        {
+            desc:"This is just a random description for the project",
+            fileSize: ".9mb",
+            close: true,
+            tag:{
+                isOpen:false , tagTitle:"Download Now", tagColor:"green"},
+        },
+    ];
+
   return (
-    <div className="fixed top-0 left-0 w-full h-full f z-[5]">
-        <Card/>
-    
+    <div className="fixed top-0 left-0 w-full h-full f z-[5] flex gap-5 flex-wrap p-5">
+        {data.map((item, index)=>(
+            <Card data={item}/>
+        ))}
     </div>
   )
 }
