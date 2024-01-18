2ed6c233e8b0af869f33fa5af54cbdb8a772b4db
farruop <farhankhanpvt00@gmail.com>
2024-01-18 23:12:03 +0530

    CUSTOMISED some leftovers,texts

--- Docs_REACT/src/components/Background.jsx
+++ Docs_REACT/src/components/Background.jsx
@@ -4,7 +4,7 @@ function background() {
   return (
     <div className='fixed w-full h-screen z-[2]'>
     <div className='flex justify-center top-[5%] w-full  mt-5 py-5 font-semibold text-xl'>Documnets</div>
-       <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-bold text-[20vw] text-zinc-900'>DOCS</h1>
+       <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-bold text-[15vw] text-zinc-900'>R.DOCS</h1>
     </div>
   )
 }
